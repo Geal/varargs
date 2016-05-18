170511b3e7c6580a812fe302d3ae3d953180f29f
Geoffroy Couprie <geo.couprie@gmail.com>
2016-05-18 15:38:20 +0200

    test va_copy

--- src/wrapper.c
+++ src/wrapper.c
@@ -1,7 +1,10 @@
 #include <stdarg.h>
 
 void transform(va_list* list, ...) {
-  va_start(*list, list);
+  va_list vl;
+  va_start(vl, list);
+  va_copy(vl, *list);
+  va_end(vl);
 }
 
 void end(va_list* list) {
